3c9495b39f7f6638607da4a479fe468b21bfed0a
KC <krkeco@gmail.com>
2019-02-18 06:08:25 -0800

    add faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+20.times do
+	User.new(
+		name: Faker::name,
+		description: Faker::Quote.famous_last_words,
+		avatar: Faker::LoremPixel.image
+
+		)
+end
